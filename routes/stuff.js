bbde8af250a0f659f28c6669901001c8fa4bef0c
Andrea G <andreagrasso.ag@live.it>
2020-07-02 23:49:55 +0200

    followed the course of will

---
+++ routes/stuff.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const stuffCtrl = require('../controllers/stuff')
+const auth = require('../middleware/auth')
+const multer = require('../middleware/multer-config');
+
+router.post('/', auth, multer, stuffCtrl.createSauce);
+router.put('/:id', auth, multer, stuffCtrl.modifySauce);
+router.delete('/:id', auth, stuffCtrl.deleteSauce);
+router.get('/:id', auth, stuffCtrl.getOneSauce);
+router.get('/', auth, stuffCtrl.getAllSauces);
+
+module.exports = router;
